9a59e1305fd5f69fe1ee159cd58e411d2ff8f2ab
Lelius <leliuslogunov@gmail.com>
2021-04-10 11:09:20 +0300

    Добавил класс начальных настроек игры. Меняется цвет окна.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 # pylint: disable=no-member
 
 class AlienInvasion:
@@ -8,7 +9,9 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -18,6 +21,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
