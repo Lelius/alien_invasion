3e18061bee64c93040a88d4fe55489490a6dcd6a
Lelius <leliuslogunov@gmail.com>
2021-04-11 18:10:19 +0300

    Ввод нового класса Bullet для снарядов. Описание параметров снарядов в Setting. Новый снаряд при нажатии клавиши Space.  Множество снарядов в группе спрайтов. Удаление снарядов, вышедших за пределы экрана.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 # pylint: disable=no-member
 
 class AlienInvasion:
@@ -12,20 +13,31 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        #self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         #Вариант полноэкранного режима
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            #Удаление снарядов, вышедших за край экрана
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            #Проверка на количество текущщих снарядов
+            #print(len(self.bullets))
+
             self._update_screen()
 
             pygame.display.flip()
@@ -48,6 +60,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self.fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -55,10 +69,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Обновляет изображение на экране и выводит экран"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
